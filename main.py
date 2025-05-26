9391eb44197411ca7d3483640d19d402138c140c
stefancyliax <stefan.cyliax@gmail.com>
2025-05-26 14:27:06 +0200

    added creation of sub sites

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from block import *
 import os
-from helper import copy_folder_contents
+from helper import copy_folder_contents, list_files_in_directory
 
 def main():
     print("Hello from blog!")
@@ -32,12 +32,25 @@ sone some more text here.
 
     md_content = read_file("content/index.md")
     doc = MarkdownParser(md_content)
-    
-    print("###################################")
-
-
-
     doc.generate_page("static/template.html","docs/index.html")
+    print("###################################")
+    
+    md_contact = read_file("content/contact/contact.md")
+    doc = MarkdownParser(md_contact)
+    os.makedirs(f"docs/contact", exist_ok=True)
+    doc.generate_page("static/template.html", "docs/contact/index.html")
+
+    article_list = list_files_in_directory("content/articles", "*.md")
+    debug_print(f"Found {len(article_list)} articles.")
+    for article in article_list:
+        print(f"Processing article: {article}")
+        content = read_file(article)
+        doc = MarkdownParser(content)
+        folder_name = os.path.basename(article).replace('.md', '')
+        os.makedirs(f"docs/blog/{folder_name}", exist_ok=True)
+        doc.generate_page("static/template.html", f"docs/blog/{folder_name}/index.html")
+
+    #TODO: referencing the .css in the subfolders is not working
 
     # t = TextParser(md3)
     # print("Parsed Markdown:")
